ce96a913ffec5c32a668f0a2f11e9e06f229e22e
Cosku Bas <cosku.bas@gmail.com>
2015-09-01 10:20:34 +0000

    Fix white space

--- scripts/configure_owncloud.php
+++ scripts/configure_owncloud.php
@@ -11,9 +11,7 @@ $CONFIG['asset-pipeline.enabled'] = true;
 $CONFIG['logfile'] = '/usr/share/nginx/html/owncloud/logs/owncloud.log';
 unset($CONFIG['overwrite.cli.url']);
 
-
 $content = '<?php' . PHP_EOL . '$CONFIG = ' . var_export($CONFIG, true) . ';';
-
 file_put_contents($filename, $content);
 
 ?>
