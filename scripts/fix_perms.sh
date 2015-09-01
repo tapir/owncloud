b54e9717c39ee09f085964861515f2868849a9e8
Cosku Bas <cosku.bas@gmail.com>
2015-09-01 16:08:05 +0300

    Removed by mistake. Readded.

--- scripts/fix_perms.sh
+++ scripts/fix_perms.sh
@@ -7,11 +7,13 @@ htgroup='www-data'
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
 find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 chown -R root:${htuser} ${ocpath}/
+chown -R root:${htuser} /var/lib/owncloud/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
 chown -R ${htuser}:${htgroup} ${ocpath}/themes/
 chown -R ${htuser}:${htgroup} ${ocpath}/assets/
-chown -R ${htuser}:${htgroup} /${ocpath}/logs/
+chown -R ${htuser}:${htgroup} ${ocpath}/logs/
 chown -R ${htuser}:${htgroup} /var/lib/owncloud/data/
 chown root:${htuser} ${ocpath}/.htaccess
 chmod 0644 ${ocpath}/.htaccess
+chmod -R 0750 /var/lib/owncloud
